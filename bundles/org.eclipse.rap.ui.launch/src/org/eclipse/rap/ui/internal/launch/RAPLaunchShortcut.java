3329b7bb464216c9f2bb99a32550529d7de75c5f
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2009-02-12 19:26:48 +0000

    RESOLVED 263483: [Tooling] Launch as RAP application fails to set VM arguments

--- bundles/org.eclipse.rap.ui.launch/src/org/eclipse/rap/ui/internal/launch/RAPLaunchShortcut.java
+++ bundles/org.eclipse.rap.ui.launch/src/org/eclipse/rap/ui/internal/launch/RAPLaunchShortcut.java
@@ -1,35 +1,34 @@
 /*******************************************************************************
- * Copyright (c) 2007 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2007 2009 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
  ******************************************************************************/
-
 package org.eclipse.rap.ui.internal.launch;
 
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.ILaunchShortcut;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.pde.ui.launcher.AbstractLaunchShortcut;
+import org.eclipse.pde.ui.launcher.OSGiLaunchShortcut;
 import org.eclipse.ui.IEditorPart;
 
 
-public final class RAPLaunchShortcut 
-  extends AbstractLaunchShortcut 
-  implements ILaunchShortcut 
+public final class RAPLaunchShortcut
+  extends OSGiLaunchShortcut
+  implements ILaunchShortcut
 {
 
-  private static final String LAUNCH_CONFIGURATION_TYPE 
+  private static final String LAUNCH_CONFIGURATION_TYPE
     = "org.eclipse.rap.ui.launch.RAPLauncher"; //$NON-NLS-1$
 
   /////////////////////////////////
   // ILaunchShortcut implementation
-  
+
   public void launch( final ISelection selection, final String mode ) {
     launch( mode );
   }
@@ -40,14 +39,15 @@ public final class RAPLaunchShortcut
 
   ///////////////////////////////////
   // AbstractLaunchShortcut overrides
-  
+
   protected String getLaunchConfigurationTypeName() {
     return LAUNCH_CONFIGURATION_TYPE;
   }
 
-  protected void initializeConfiguration( 
-    final ILaunchConfigurationWorkingCopy workingCopy ) 
+  protected void initializeConfiguration(
+    final ILaunchConfigurationWorkingCopy workingCopy )
   {
+    super.initializeConfiguration( workingCopy );
   }
 
   protected boolean isGoodMatch( final ILaunchConfiguration config ) {
