ec324346dfaa22578d3b1ca2f66127ca08c3b25d
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2009-02-12 19:24:50 +0000

    Renamed templates folder

--- bundles/org.eclipse.rap.ui.templates/src/org/eclipse/rap/internal/ui/templates/rap/AbstractRAPTemplate.java
+++ bundles/org.eclipse.rap.ui.templates/src/org/eclipse/rap/internal/ui/templates/rap/AbstractRAPTemplate.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2007 Innoopract Informationssysteme GmbH.
+ * Copyright (c) 2007, 2009 Innoopract Informationssysteme GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     Innoopract Informationssysteme GmbH - initial API and implementation
  ******************************************************************************/
@@ -29,7 +29,7 @@ abstract class AbstractRAPTemplate extends OptionTemplateSection {
 
   protected static final String KEY_APPLICATION_CLASS = "applicationClass"; //$NON-NLS-1$
   protected static final String KEY_WINDOW_TITLE = "windowTitle"; //$NON-NLS-1$
-  protected static final String TEMPLATES_DIRECTORY = "templates_3.2"; //$NON-NLS-1$
+  protected static final String TEMPLATES_DIRECTORY = "templates"; //$NON-NLS-1$
 
   protected final URL getInstallURL() {
     return TemplateUtil.getInstallURL();
@@ -57,22 +57,23 @@ abstract class AbstractRAPTemplate extends OptionTemplateSection {
     initializeOption( KEY_PACKAGE_NAME, packageName );
   }
 
-  protected void initializeFields(IFieldData data) {
+  protected void initializeFields( final IFieldData data ) {
     String packageName = TemplateUtil.getFormattedPackageName( data.getId() );
     initializeOption( KEY_PACKAGE_NAME, packageName );
   }
 
-  public IPluginReference[] getDependencies(final String schemaVersion) {
+  public IPluginReference[] getDependencies( final String schemaVersion ) {
     return new IPluginReference[] {
       new PluginReference( "org.eclipse.rap.ui", null, 0 ) //$NON-NLS-1$
     };
   }
 
-  // protected methods
   ////////////////////
+  // protected methods
 
-  protected final void addExtensionToPlugin( final IPluginExtension extension ) 
-  throws CoreException {
+  protected final void addExtensionToPlugin( final IPluginExtension extension )
+    throws CoreException
+  {
     IPluginBase plugin = model.getPluginBase();
     if( !extension.isInTheModel() ) {
       plugin.add( extension );
@@ -96,7 +97,7 @@ abstract class AbstractRAPTemplate extends OptionTemplateSection {
     IPluginBase plugin = model.getPluginBase();
     return plugin.getId();
   }
-  
+
   protected String getTemplateDirectory() {
     return TEMPLATES_DIRECTORY;
   }
