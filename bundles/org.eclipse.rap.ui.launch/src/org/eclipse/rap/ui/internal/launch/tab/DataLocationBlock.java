530d9a6029875c01450865ec357321ac55d157fe
Holger Staudacher <hstaudacher@eclipsesource.com>
2011-04-20 07:56:52 +0000

    Updated copyright header of DataLocationBlock

--- bundles/org.eclipse.rap.ui.launch/src/org/eclipse/rap/ui/internal/launch/tab/DataLocationBlock.java
+++ bundles/org.eclipse.rap.ui.launch/src/org/eclipse/rap/ui/internal/launch/tab/DataLocationBlock.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2011 EclipseSource.
+ * Copyright (c) 2005, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     EclipseSource - initial API and implementation
+ *     IBM Corporation - initial implementation
+ *     EclipseSource - ongoing development
  ******************************************************************************/
 package org.eclipse.rap.ui.internal.launch.tab;
 
