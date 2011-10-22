868f2712bccf664ea0c4f80b700155a9bb1d5c18
Rüdiger Herrmann <ruediger.herrmann@gmx.de>
2011-10-22 18:18:00 +0000

    Changes for bug 361730: [Tooling] Extend RWT launcher to launch from a web app folder Extracted UI for project selection to class ProjectSection

--- bundles/org.eclipse.rap.ui.launch.rwt/src/org/eclipse/rap/ui/internal/launch/rwt/tab/RWTMainTab.java
+++ bundles/org.eclipse.rap.ui.launch.rwt/src/org/eclipse/rap/ui/internal/launch/rwt/tab/RWTMainTab.java
@@ -10,30 +10,26 @@
  ******************************************************************************/
 package org.eclipse.rap.ui.internal.launch.rwt.tab;
 
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
-import org.eclipse.jdt.core.*;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.rap.ui.internal.launch.rwt.config.RWTLaunchConfig;
 import org.eclipse.rap.ui.internal.launch.rwt.util.Images;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.widgets.Composite;
 
 
 public class RWTMainTab extends RWTLaunchTab {
 
-  private Text txtProject;
-  private Button btnSelectProject;
+  private ProjectSection projectSection;
   private EntryPointSection entryPointSection;
   private RuntimeSettingsSection runtimeSettingsSection;
   private BrowserSection  browserSection;
   
   public RWTMainTab() {
+    projectSection = new ProjectSection();
     entryPointSection = new EntryPointSection();
     runtimeSettingsSection = new RuntimeSettingsSection();
     browserSection = new BrowserSection();
@@ -51,7 +47,7 @@ public class RWTMainTab extends RWTLaunchTab {
     Composite container = new Composite( parent, SWT.NONE );
     container.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ) );
     container.setLayout( new GridLayout( 1, false ) );
-    createProjectSection( container );
+    projectSection.createControl( container );
     entryPointSection.createControl( container );
     runtimeSettingsSection.createControl( container );
     browserSection.createControl( container );
@@ -60,81 +56,25 @@ public class RWTMainTab extends RWTLaunchTab {
     HelpContextIds.assign( getControl(), HelpContextIds.MAIN_TAB );
   }
 
-  private void createProjectSection( Composite container ) {
-    Group group = new Group( container, SWT.NONE );
-    group.setText( "Project" );
-    group.setLayoutData( new GridData( SWT.FILL, SWT.TOP, true, false ) );
-    group.setLayout( new GridLayout( 2, false ) );
-    txtProject = new Text( group, SWT.BORDER );
-    txtProject.setLayoutData( new GridData( SWT.FILL, SWT.TOP, true, false ) );
-    txtProject.addModifyListener( new TextModifyListener() );
-    btnSelectProject = new Button( group, SWT.PUSH );
-    btnSelectProject.setText( "&Browse..." );
-    GridData gridData = new GridData( SWT.LEFT, SWT.TOP, false, false );
-    btnSelectProject.setLayoutData( gridData );
-    btnSelectProject.addSelectionListener( new JavaProjectSelectionListener() );
-  }
-
   public void setLaunchConfigurationDialog( ILaunchConfigurationDialog dialog ) {
     super.setLaunchConfigurationDialog( dialog );
+    
     entryPointSection.setLaunchConfigurationDialog( dialog );
     runtimeSettingsSection.setLaunchConfigurationDialog( dialog );
     browserSection.setLaunchConfigurationDialog( dialog );
   }
 
   public void initializeFrom( RWTLaunchConfig launchConfig ) {
-    txtProject.setText( launchConfig.getProjectName() );
+    projectSection.initializeFrom( launchConfig );
     entryPointSection.initializeFrom( launchConfig );
     runtimeSettingsSection.initializeFrom( launchConfig );
     browserSection.initializeFrom( launchConfig );
   }
 
   public void performApply( RWTLaunchConfig launchConfig ) {
-    launchConfig.setProjectName( txtProject.getText().trim() );
+    projectSection.performApply( launchConfig );
     entryPointSection.performApply( launchConfig );
     runtimeSettingsSection.performApply( launchConfig );
     browserSection.performApply( launchConfig );
-  }
-  
-  private void handleSelectJavaProject() {
-    IJavaProject project = selectJavaProject();
-    if( project != null ) {
-      String projectName = project.getElementName();
-      txtProject.setText( projectName );
-    }
-  }
-
-  private IJavaProject selectJavaProject() {
-    Shell shell = getShell();
-    JavaProjectSelectionDialog dialog = new JavaProjectSelectionDialog( shell );
-    dialog.setInitialSelection( getJavaProject() );
-    return dialog.open();
-  }
-
-  private IJavaProject getJavaProject() {
-    IJavaProject result = null;
-    String projectName = txtProject.getText().trim();
-    if( projectName.length() > 0 ) {
-      result = getJavaModel().getJavaProject( projectName );
-    }
-    return result;    
-  }
-
-  private static IJavaModel getJavaModel() {
-    IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-    return JavaCore.create( workspaceRoot );
-  }
-  
-  private class JavaProjectSelectionListener extends SelectionAdapter {
-    public void widgetSelected( SelectionEvent event ) {
-      handleSelectJavaProject();
-      updateLaunchConfigurationDialog();
-    }
-  }
-  
-  private class TextModifyListener implements ModifyListener {
-    public void modifyText( ModifyEvent event ) {
-      updateLaunchConfigurationDialog();
-    }
   }
 }
