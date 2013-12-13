a8931fb2618c0cd86d37e628ce09d80b2f43ff3d
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-12-13 17:18:55 +0100

    Copy legal files in maven build, cleanup build script
    * Use ant task for copying legal files into repository.
      This must be done in prepare-package. If it's done in package,
      the files are not included in the zip, if it's done too early,
      the target/repository directory is overwritten.
      I've opened bug 424691 to request a config option in tycho to
      include those files.
    
    * Clean up obsolete steps from build file
      - cleanup workspace - now done by the hudson jobs
      - clean up local Maven repository - every hudson job starts with
        a new Maven repo now
      - Obsolete signing steps - signing and pack200 is now done on a
        per-plug-in basis using CBI plugins
      - Include legal files in zip
    
    * Remove README. This file contained mostly obvious information
      and was even wrong in suggesting to use `mvn clean package`
      which will skip tests.
      It will be replaced by a top-level README file (bug 424687).
    
    Change-Id: I41f7ec5933ab4734fcc3780dc08e0a74135dd02a

--- releng/org.eclipse.rap.tools.build/scripts/tools-build.sh
+++ releng/org.eclipse.rap.tools.build/scripts/tools-build.sh
@@ -4,9 +4,7 @@
 # All values are retrieved trough system variables set by Hudson.
 # See Job -> Configure... -> This build is parameterized
 
-SCRIPTS_DIR=$(dirname $(readlink -nm $0))
 MVN=${MVN:-"/opt/public/common/apache-maven-3.0.4/bin/mvn"}
-MAVEN_LOCAL_REPO_PATH=${MAVEN_LOCAL_REPO_PATH:-"/shared/rt/rap/m2/repository"}
 
 if [ "${BUILD_TYPE:0:1}" == "S" ]; then
   sign=true
@@ -15,24 +13,14 @@ else
 fi
 
 ######################################################################
-# Cleanup left-overs from previous run
-
-test -d "$WORKSPACE" || exit 1
-rm -rf "$WORKSPACE"/*.zip
-
-######################################################################
-# clean up local Maven repository to circumvent p2 cache problems
-for II in .cache .meta p2 ; do
-  echo "Remove directory ${MAVEN_LOCAL_REPO_PATH}/${II}" 
-  rm -r ${MAVEN_LOCAL_REPO_PATH}/${II}
-done
-
-######################################################################
 # Build RAP Tools
 
 cd "$WORKSPACE/org.eclipse.rap.tools/releng/org.eclipse.rap.tools.build" || exit 1
 echo "Running maven on $PWD, sign=$sign"
 $MVN clean integration-test -Dsign=$sign || exit 1
+
+######################################################################
+# Rename zip file
 
 VERSION=$(ls repository/target/repository/features/org.eclipse.rap.tools.feature_*.jar | sed 's/.*_\([0-9.-]\+\)\..*\.jar/\1/')
 TIMESTAMP=$(ls repository/target/repository/features/org.eclipse.rap.tools.feature_*.jar | sed 's/.*\.\([0-9-]\+\)\.jar/\1/')
@@ -41,29 +29,5 @@ echo "Timestamp is '$TIMESTAMP'"
 test -n "$VERSION" || exit 1
 test -n "$TIMESTAMP" || exit 1
 
-# Example: rap-tools-1.5.0-N-20110814-2110.zip
-zipFileName=rap-tools-$VERSION-$BUILD_TYPE-$TIMESTAMP.zip
-if [ -d repository/target/fixedSigned ]; then
-  cd repository/target/fixedSigned
-  zip -r "$WORKSPACE/$zipFileName" .
-  zip -d "$WORKSPACE/$zipFileName" "META-INF/*"
-  cd -
-else
-  mv repository/target/*.zip "$WORKSPACE/$zipFileName"
-fi
-
-repoZipFileName=rap-tools-repo-$VERSION-$BUILD_TYPE-$TIMESTAMP.zip
-if [ -d repository/target/fixedPacked ]; then
-  cd repository/target/fixedPacked
-  zip -r "$WORKSPACE/$repoZipFileName" .
-  zip -d "$WORKSPACE/$repoZipFileName" "META-INF/*"
-  cd -
-fi
-
-######################################################################
-# Include legal files in zip
-
-cd "$WORKSPACE"
-cp -f org.eclipse.rap.tools/releng/org.eclipse.rap.tools.build/legal/notice.html .
-cp -f org.eclipse.rap.tools/releng/org.eclipse.rap.tools.build/legal/epl-v10.html .
-zip "$zipFileName" notice.html epl-v10.html
+# Example: rap-tools-2.2.0-N-20110814-2110.zip
+mv repository/target/*.zip "$WORKSPACE/rap-tools-$VERSION-$BUILD_TYPE-$TIMESTAMP.zip"
