482eff8565b1ffca04de50988f34f9db8123ac4c
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-01-04 11:24:17 +0100

    Add build qualifier to repository zip
    Use the tycho property ${buildQualifier} in the repository zip
    filename. In our build, this qualifier is generated by the tycho-
    buildtimestamp-jgit plug-in. For eclipse-repository projects, it
    is apparently bound to the included feature's build qualifiers.
    
    With this change, the build script becomes mostly redundant and
    should be deleted once the hudson build doesn't need it anymore.
    
    Change-Id: I25d12de9b7d5a9225324d197815946729e4166d2

--- releng/org.eclipse.rap.tools.build/scripts/tools-build.sh
+++ releng/org.eclipse.rap.tools.build/scripts/tools-build.sh
@@ -12,22 +12,8 @@ else
   sign=false
 fi
 
-######################################################################
-# Build RAP Tools
-
 cd "$WORKSPACE/org.eclipse.rap.tools/releng/org.eclipse.rap.tools.build" || exit 1
 echo "Running maven on $PWD, sign=$sign"
 $MVN clean integration-test -Dsign=$sign || exit 1
 
-######################################################################
-# Rename zip file
-
-VERSION=$(ls repository/target/repository/features/org.eclipse.rap.tools.feature_*.jar | sed 's/.*_\([0-9.-]\+\)\..*\.jar/\1/')
-TIMESTAMP=$(ls repository/target/repository/features/org.eclipse.rap.tools.feature_*.jar | sed 's/.*\.\([0-9-]\+\)\.jar/\1/')
-echo "Version is '$VERSION'"
-echo "Timestamp is '$TIMESTAMP'"
-test -n "$VERSION" || exit 1
-test -n "$TIMESTAMP" || exit 1
-
-# Example: rap-tools-2.2.0-N-20110814-2110.zip
-mv repository/target/*.zip "$WORKSPACE/rap-tools-$VERSION-$BUILD_TYPE-$TIMESTAMP.zip"
+mv repository/target/*.zip "$WORKSPACE"
