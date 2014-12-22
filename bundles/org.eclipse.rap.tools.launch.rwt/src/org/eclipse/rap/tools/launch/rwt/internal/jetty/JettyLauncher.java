d73257278f86481a4687de4c60b37a9d64b37458
Ivan Furnadjiev <ivan@eclipsesource.com>
2014-12-22 16:31:01 +0200

    Update RAP Tools to Jetty 9.2.5.201411141551
    Simplify JettyLauncher as recomended in
    https://www.eclipse.org/jetty/documentation/current/
    embedded-examples.html#embedded-one-webapp 
    
    Change-Id: Iaa620bfd07b3ca0613ba640811a0cf702b11990c
    Signed-off-by: Ivan Furnadjiev <ivan@eclipsesource.com>

--- bundles/org.eclipse.rap.tools.launch.rwt/src/org/eclipse/rap/tools/launch/rwt/internal/jetty/JettyLauncher.java
+++ bundles/org.eclipse.rap.tools.launch.rwt/src/org/eclipse/rap/tools/launch/rwt/internal/jetty/JettyLauncher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 EclipseSource and others.
+ * Copyright (c) 2012, 2014 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,10 +10,7 @@
  ******************************************************************************/
 package org.eclipse.rap.tools.launch.rwt.internal.jetty;
 
-import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.bio.SocketConnector;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -22,18 +19,11 @@ public class JettyLauncher {
 
   public static void main( String[] args ) {
     try {
-      Server server = new Server();
-      ContextHandlerCollection contexts = new ContextHandlerCollection();
-      server.setHandler( contexts );
-      SocketConnector connector = new SocketConnector();
-      connector.setPort( Integer.parseInt( args[ 0 ] ) );
-      server.setConnectors( new Connector[]{ connector } );
-
+      Server server = new Server( Integer.parseInt( args[ 0 ] ) );
       WebAppContext webapp = new WebAppContext();
       webapp.setContextPath( args[ 1 ] );
       webapp.setWar( args[ 2 ] );
-      contexts.addHandler(webapp);
-
+      server.setHandler( webapp );
       server.start();
       server.join();
     } catch( Exception exception ) {
